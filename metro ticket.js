0f90329d1c7d661fcbee34ece557a5e526eca852
prathamjoshi-1 <mr.prathamjoshi1@gmail.com>
2025-08-26 10:26:34 -0700

    Create metro ticket.js

---
+++ metro ticket.js
@@ -0,0 +1,19 @@
+
+        let age=7;
+        let gender='male'; // Male and Female
+        let finalDiscount;
+
+        if(age<=5){
+            finalDiscount=100;
+        }
+        else if(gender==='female'|| age<=8){
+            finalDiscount=50;
+        }
+        else if(age>=65){
+            finalDiscount=30;
+        }else{
+            finalDiscount=0;
+        }
+
+        console.log(`Your Final Discount is ${finalDiscount} %`);
+  
