d0e5b879fe749969506b5a06fc43f9170ec4ebb7
prathamjoshi-1 <mr.prathamjoshi1@gmail.com>
2025-08-26 10:00:41 -0700

    Create Bat Ball Stump.js

---
+++ Bat Ball Stump.js
@@ -0,0 +1,32 @@
+<script>
+    function playGame(userChoice) {
+      const userChoiceMessage = `You Have Chosen ${userChoice}`;
+      
+      let randomNum = Math.random() * 3;
+      let computerChoice;
+      if (randomNum <= 1) {
+        computerChoice = 'Bat';
+      } else if (randomNum <= 2) {
+        computerChoice = 'Ball';
+      } else {
+        computerChoice = 'Stump';
+      }
+      
+      const compChoiceMessage = `-> Computer Choice : ${computerChoice}`;
+      let resultMessage;
+
+      if (userChoice === computerChoice) {
+        resultMessage = '-> It is Tie.';
+      } else if (
+        (userChoice === 'Bat' && computerChoice === 'Ball') ||
+        (userChoice === 'Ball' && computerChoice === 'Stump') ||
+        (userChoice === 'Stump' && computerChoice === 'Bat')
+      ) {
+        resultMessage = '-> User Won.';
+      } else {
+        resultMessage = '-> Computer Won.';
+      }
+
+      alert(`${userChoiceMessage} ${compChoiceMessage} ${resultMessage}`);
+    }
+  </script>
