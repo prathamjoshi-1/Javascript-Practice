d9ed0e2ee8348970416fe746ac0236b1e2da653f
prathamjoshi-1 <mr.prathamjoshi1@gmail.com>
2025-08-26 10:19:25 -0700

    Create javascript chapter3.js

---
+++ javascript chapter3.js
@@ -0,0 +1,8 @@
+<script>
+        console.log("Pratham");
+        alert("Welcome Buddy");
+        console.log(2+2);
+        console.log('Good Morning'+'Pratham');
+        console.warn("This is a Warning");
+         console.error("Stop-This.This is an Error");
+    </script>
